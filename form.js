71a7d1e2fee9759297f954acda9c1e8db90cb79e
Brendan Paing <brendanpaing@gmail.com>
2022-06-29 19:29:17 -0700

    add password matching validation

--- form.js
+++ form.js
@@ -1,1 +1,17 @@
-const submit = document.querySelector('button[type=\'submit\']');
+const form = document.querySelector('#signup');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirmPassword');
+
+function passwordsMatch() {
+    return password.value.trim() === confirmPassword.value.trim();
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let validForm = passwordsMatch();
+    if (!validForm) {
+        password.classList.add('error');
+        confirmPassword.classList.add('error');
+        password.focus();
+    }
+});
